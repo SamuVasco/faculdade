6327afe5e2e8f39e5054add450043a8159d44907
Samuel Vasco <127765003+SamuVasco@users.noreply.github.com>
2026-09-23 14:01:05 -0300

    .

---
+++ Programação/C/TDE/2 periodo/tdePonteiros.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+
+int main(void){
+	
+	int vet[5] = {10, 20, 30, 40, 50};
+	int *p = vet;
+	
+	printf("%i", *p+1);
+	
+	
+	return 0;	
+}
--- Programação/C/TDE/2 periodo/teste.cpp
+++ Programação/C/TDE/2 periodo/teste.cpp
@@ -1,14 +1,34 @@
 #include <stdio.h>
 
+void multiplicar_escalar(int *v1, int *v2, int x, int n);
 
 int main(void){
     
-    int x = 10;
-    int *p = &x;
-    
-    printf("%i", *p);
-
+	int v1[3] = {1,2,3}, v2[3], x = 1, n = 0;
+	
+	n = sizeof(v1) / sizeof(v1[0]); // numero de elementos dentro do v1
+	
+	printf("\nDigite o valor do escalar: ");
+	scanf("%i", &x);
+	
+	printf("\nVetor v1: v1[3] = {1,2,3}\n");
+	
+	printf("\nResultado\n");
+	
+	multiplicar_escalar(v1, v2, x, n);
+	
+	for(int i = 0; i < n; i++){
+		printf("%i ", *(v2 + i));
+	}
+	
     return 0;
+}
+
+void multiplicar_escalar(int *v1, int *v2, int x, int n){
+	
+	for(int i = 0; i < n; i++){
+		*(v2 + i) = *(v1 + i) * x;
+	}
 }
 
 
